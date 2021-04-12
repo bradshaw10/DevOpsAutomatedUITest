fefa9ab0eefcdab5f7162e235815694071ece93c
bradshaw10 <eoghan.bradshaw@gmail.com>
2021-04-12 14:55:12 +0100

    Initial commit

---
+++ CreditCards.UITests/CreditCardWebAppShould.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CreditCards.UITests
+{
+    public class CreditCardWebAppShould
+    {
+        private const string homeUrl = "http://localhost:44108/";
+        private const string homeTitle = "Home Page - Credit Cards";
+        private const string aboutUrl = "http://localhost:44108/Home/About"; 
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(homeUrl);
+
+                DemoHelper.Pause();
+
+                Assert.Equal(homeTitle, driver.Title);
+                Assert.Equal(homeUrl, driver.Url);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void ReloadApplicationPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(homeUrl);
+
+                DemoHelper.Pause();
+
+                driver.Navigate().Refresh();
+                Assert.Equal(homeTitle, driver.Title);
+                Assert.Equal(homeUrl, driver.Url);
+            }
+        }
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void ReloadHomePageOnBackward()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(aboutUrl);
+                DemoHelper.Pause();
+
+                driver.Navigate().GoToUrl(homeUrl);
+                DemoHelper.Pause();
+
+                driver.Navigate().Back();
+                DemoHelper.Pause();
+
+                driver.Navigate().Forward();
+                DemoHelper.Pause();
+
+                Assert.Equal(homeTitle, driver.Title);
+                Assert.Equal(homeUrl, driver.Url);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void ReloadHomePageOnForward()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(homeUrl);
+                DemoHelper.Pause();
+
+                driver.Navigate().Refresh();
+                Assert.Equal(homeTitle, driver.Title);
+                Assert.Equal(homeUrl, driver.Url);
+            }
+        }
+    }
+}
